27d57cd7785ea401d804c076138fde96e604fc5d
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-12-29 11:32:56 +0100

    ..

--- src/mkl/mkl_sgemm.cpp
+++ src/mkl/mkl_sgemm.cpp
@@ -10,7 +10,7 @@
 unsigned int numThreads = std::thread::hardware_concurrency();
 
 // Blocked MMul benchmark
-static void blocked_mmul_bench(benchmark::State &s) {
+static void blocked_aligned_mmul_bench(benchmark::State &s) {
   // Number Dimensions of our matrix
   std::size_t N = s.range(0);
 
@@ -43,6 +43,47 @@ static void blocked_mmul_bench(benchmark::State &s) {
   mkl_free(A);
   mkl_free(B);
   mkl_free(C);
+}
+BENCHMARK(blocked_aligned_mmul_bench)
+    ->Arg(2*numThreads*16)
+    ->Arg(4*numThreads*16)
+    ->Arg(6*numThreads*16)
+    ->Unit(benchmark::kMillisecond);
+
+// Blocked MMul benchmark
+static void blocked_mmul_bench(benchmark::State &s) {
+  // Number Dimensions of our matrix
+  std::size_t N = s.range(0);
+
+  // Create our random number generators
+  std::mt19937 rng;
+  rng.seed(std::random_device()());
+  std::uniform_real_distribution<float> dist(-10, 10);
+
+  // Create input matrices
+  float *A = (float *)malloc(N * N * sizeof(float));
+  float *B = (float *)malloc(N * N * sizeof(float));
+  float *C = (float *)malloc(N * N * sizeof(float));
+
+  // MMul scaling constants
+  float alpha = 1.0;
+  float beta = 0.0;
+
+  // Initialize them with random values (and C to 0)
+  std::generate(A, A + N * N, [&] { return dist(rng); });
+  std::generate(B, B + N * N, [&] { return dist(rng); });
+  std::generate(C, C + N * N, [&] { return 0; });
+
+  // Main benchmark loop
+  for (auto _ : s) {
+    cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, N, N, N, alpha, A, N,
+                B, N, beta, C, N);
+  }
+
+  // Free memory
+  free(A);
+  free(B);
+  free(C);
 }
 BENCHMARK(blocked_mmul_bench)
     ->Arg(2*numThreads*16)
