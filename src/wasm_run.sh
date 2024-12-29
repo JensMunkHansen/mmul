933d9816e0701939393aebe1cb251211dd913b49
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-12-29 16:41:34 +0100

    ..

--- src/wasm_run.sh
+++ src/wasm_run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-node build/baseline/mmul_bench.cjs --benchmark_min_time=3.0s
-node build/baseline_nonpower/mmul_bench.cjs --benchmark_min_time=3.0s
-node build/blocked/mmul_bench.cjs --benchmark_min_time=3.0s
-node build/blocked_column/mmul_bench.cjs --benchmark_min_time=3.0s
-node build/blocked_column_multi_output/mmul_bench.cjs --benchmark_min_time=3.0s
+node build/baseline/mmul_bench.cjs --benchmark_min_time=5.0s
+node build/baseline_nonpower/mmul_bench.cjs --benchmark_min_time=5.0s
+node build/blocked/mmul_bench.cjs --benchmark_min_time=5.0s
+node build/blocked_column/mmul_bench.cjs --benchmark_min_time=5.0s
+node build/blocked_column_multi_output/mmul_bench.cjs --benchmark_min_time=5.0s
 
 # Linear memory is divided into pages of 64 kB each 
 
